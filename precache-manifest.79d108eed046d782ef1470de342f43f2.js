f1329501940fc8c666180c8156dbb3b71cb07fcb
NicolasGil <nick.gil360@gmail.com>
2022-08-03 16:19:37 -0400

    Updates

---
+++ precache-manifest.79d108eed046d782ef1470de342f43f2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "40da772a27ee59d73787",
+    "url": "/stage_react_api/static/css/main.23d3016a.chunk.css"
+  },
+  {
+    "revision": "40da772a27ee59d73787",
+    "url": "/stage_react_api/static/js/main.40da772a.chunk.js"
+  },
+  {
+    "revision": "eb16bab35ee43702f8a6",
+    "url": "/stage_react_api/static/js/1.eb16bab3.chunk.js"
+  },
+  {
+    "revision": "a0a858ac8b6f4e7c6b88",
+    "url": "/stage_react_api/static/js/runtime~main.a0a858ac.js"
+  },
+  {
+    "revision": "2a888afa9934d6b603b398bb8d6d4e6e",
+    "url": "/stage_react_api/static/media/not-found.2a888afa.svg"
+  },
+  {
+    "revision": "8f424d3a2f8d5a0bc47c72c3c4b93b66",
+    "url": "/stage_react_api/static/media/warning.8f424d3a.png"
+  },
+  {
+    "revision": "bceb14e3beac395c94882f22a8f0a013",
+    "url": "/stage_react_api/static/media/NewsExplorer-White.bceb14e3.svg"
+  },
+  {
+    "revision": "7d75bcdd4b98e7f58c5b79dd6515f4bf",
+    "url": "/stage_react_api/static/media/logout-white.7d75bcdd.svg"
+  },
+  {
+    "revision": "cba355a5a8526b743d0a3d4ccee217f2",
+    "url": "/stage_react_api/static/media/NewsExplorer-Black.cba355a5.svg"
+  },
+  {
+    "revision": "9cbc93bd00dfb46e459b00acbef89505",
+    "url": "/stage_react_api/static/media/logout-black.9cbc93bd.svg"
+  },
+  {
+    "revision": "864bab35c049a0b2c91b4bde4a671ccf",
+    "url": "/stage_react_api/static/media/author.864bab35.jpg"
+  },
+  {
+    "revision": "61c63b32d79101c66262ef9a93be4b24",
+    "url": "/stage_react_api/static/media/github.61c63b32.svg"
+  },
+  {
+    "revision": "65c06f96a209c178f225d8e8691dcd33",
+    "url": "/stage_react_api/static/media/header_background.65c06f96.png"
+  },
+  {
+    "revision": "41f7f6baf365cca4c981e3993a90d439",
+    "url": "/stage_react_api/static/media/hamburger-close.41f7f6ba.svg"
+  },
+  {
+    "revision": "5b1deb62f0b626b2a805150a0134f7d8",
+    "url": "/stage_react_api/static/media/hamburger-white.5b1deb62.svg"
+  },
+  {
+    "revision": "38eabdd440b0796984612a8597d96045",
+    "url": "/stage_react_api/static/media/hamburger-black.38eabdd4.svg"
+  },
+  {
+    "revision": "0cba6a4c63d7606ac651cf707b6d6aca",
+    "url": "/stage_react_api/static/media/close-button.0cba6a4c.svg"
+  },
+  {
+    "revision": "75c2918212db1e8eed5f6a1585a12955",
+    "url": "/stage_react_api/static/media/bookmark-black.75c29182.svg"
+  },
+  {
+    "revision": "5e81c92ba0b6079e8ce0be81d0b246f8",
+    "url": "/stage_react_api/static/media/bookmark-gray.5e81c92b.svg"
+  },
+  {
+    "revision": "05f1325df308d7266a2d4e4a44b01760",
+    "url": "/stage_react_api/static/media/bookmark-blue.05f1325d.svg"
+  },
+  {
+    "revision": "5c12acc2d97c03493c940fee1a9a0813",
+    "url": "/stage_react_api/static/media/trash-gray.5c12acc2.svg"
+  },
+  {
+    "revision": "d8e9b6df81d3937eac86e5d2f21de626",
+    "url": "/stage_react_api/static/media/trash-black.d8e9b6df.svg"
+  },
+  {
+    "revision": "84046321eb9c50c7ab401f2905936fc6",
+    "url": "/stage_react_api/index.html"
+  }
+];
