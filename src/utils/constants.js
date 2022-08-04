9b13f042c40ffd5370ab463ad727d2a4701bbdb1
NicolasGil <nick.gil360@gmail.com>
2022-08-04 07:57:05 -0400

    add api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const apiKey = "695064ce7e354fa982d3bae257a13646";
+export const apiKey = "a497073cebc54026afb12228f6ab6a2c";
 
 export const BASE_URL = "https://nomoreparties.co/news/v2/everything";
 
